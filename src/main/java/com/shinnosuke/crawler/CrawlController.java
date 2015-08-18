a965794c1c8ed283a7d4474649659c14b0172be6
zhangxinsi1784 <zx150842@gmail.com>
2015-08-18 20:50:12 +0800

    first commit

--- src/main/java/com/shinnosuke/crawler/CrawlController.java
+++ src/main/java/com/shinnosuke/crawler/CrawlController.java
@@ -1,12 +1,39 @@
 package com.shinnosuke.crawler;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
 public class CrawlController {
 	
 	public CrawlController() {
 		
 	}
 	
-	public <T extends WebCrawler> void start(Class<T> c, int numberOfCrawler) {
+	public <T extends WebCrawler> void start(Class<T> _c, int numberOfCrawler) {
+		final List<T> crawlers = Lists.newArrayList();
+		for (int i = 1; i <= numberOfCrawler; ++i) {
+			T c;
+			try {
+				c = _c.newInstance();
+			} catch (InstantiationException | IllegalAccessException e) {
+				throw new RuntimeException(e);
+			} 
+			Thread thread = new Thread(c);
+			thread.start();
+			crawlers.add(c);
+		}
 		
+		Thread monitorThread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				for (T c : crawlers) {
+					
+				}
+				
+			}
+		});
 	}
+	
 }
--- src/main/java/com/shinnosuke/crawler/WebCrawler.java
+++ src/main/java/com/shinnosuke/crawler/WebCrawler.java
@@ -1,5 +1,11 @@
 package com.shinnosuke.crawler;
 
-public class WebCrawler {
+public class WebCrawler implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
