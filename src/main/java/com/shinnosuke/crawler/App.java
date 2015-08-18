5ac920ad9ff072c436f792efe6a8c81ad55b48f1
zhangxinsi1784 <zx150842@gmail.com>
2015-08-18 20:29:32 +0800

    first commit

---
+++ src/main/java/com/shinnosuke/crawler/App.java
@@ -0,0 +1,13 @@
+package com.shinnosuke.crawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
